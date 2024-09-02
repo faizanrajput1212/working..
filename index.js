387aad69dc6fea3f129ec0f8de76134d32d1eb4d
faizanrajput1212 <109417497+faizanrajput1212@users.noreply.github.com>
2024-09-02 12:57:55 +0500

    Update index.js

--- index.js
+++ index.js
@@ -25,9 +25,20 @@ const ftpConfig = {
   user: process.env.FUSER,
   password:process.env.FPASSWORD,
 };
-  const client = new FTPClient();
+  const [connectionStatus, setConnectionStatus] = useState('');
 
-  client.connect(ftpConfig);
+  // Function to check FTP connection
+  const checkFtpConnection = async () => {
+    try {
+      await FtpClient.connect(ftpConfig);
+      setConnectionStatus('Connected to FTP server');
+      console.log('Connected to FTP server');
+    } catch (error) {
+      setConnectionStatus('Failed to connect: ' + error.message);
+      console.error('Error connecting to FTP:', error);
+    }
+  };
+checkFTpConnection();
 app.post('/upload', upload.single('image'), (req, res) => {
  const client = new FTPClient();
 
